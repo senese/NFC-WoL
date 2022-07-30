694fca701ede23f00cf7943e54f50c6160252063
Diego Senese <dsenese@timbrasil.com.br>
2022-07-30 00:59:08 -0300

    socket fixes

--- wol.py
+++ wol.py
@@ -1,14 +1,15 @@
 import socket
 
+ADDR =  ('192.168.0.1', 7)
 header = b'\xFF\xFF\xFF\xFF\xFF\xFF'
 mac = b'\xE0\xD5\x5E\x64\x1B\xEC'
 
 magic_packet = bytearray()
 magic_packet += header
-print(magic_packet)
-for _ in range (16):
+for _ in range (15):
     magic_packet += mac
 
-s = socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect('192;168.0.1')
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(ADDR)
 s.send(magic_packet)

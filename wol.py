9afca3d677baf4c732745974152d9e9fce7758f0
Diego Senese <dsenese@timbrasil.com.br>
2022-08-07 13:24:35 -0300

    wake on lan working

--- wol.py
+++ wol.py
@@ -1,15 +1,14 @@
 import socket
 
-ADDR =  ('192.168.0.1', 7)
+ADDR =  ('255.255.255.255', 9)
 header = b'\xFF\xFF\xFF\xFF\xFF\xFF'
 mac = b'\xE0\xD5\x5E\x64\x1B\xEC'
 
 magic_packet = bytearray()
 magic_packet += header
-for _ in range (15):
+for _ in range (16):
     magic_packet += mac
 
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(ADDR)
 s.send(magic_packet)
